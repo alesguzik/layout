a42dad7cb14470240a46ce59bdbeef1df0bac4d7
Ales Guzik <me@aguzik.net>
2013-07-31 16:46:05 +0300

    first version

---
+++ defs.zsh
@@ -0,0 +1,34 @@
+PGUP=Prior
+PGDN=Next
+
+s(){ sleep "$@" }
+k(){ xdotool key --delay 100 "$@"; s 1 }
+ret(){ k Return }
+tt(){ xdotool type "$@" }
+t(){ tt "$@"; ret }
+
+sm(){ k Alt+t "$@" }
+visit(){ k Alt+Up }
+leave(){ k Alt+Down }
+next(){ k Alt+Right }
+prev(){ k Alt+Left }
+run(){
+    WAIT=$1; shift;
+    sm Shift+1;
+    t "$@";
+    s $WAIT
+}
+with_window(){ k Ctrl+Escape "$@" }
+mkframe(){ sm a Return }
+nextroot(){ sm $PGUP }
+prevroot(){ sm $PGDN }
+swaproot(){ sm Ctrl+$PGUP }
+current_root(){ k Alt+Return }
+toggleterm(){ k Ctrl+F12 }
+term(){ sm t }
+tmctl(){ echo "$@" |xargs -d ' ' -I_ echo Ctrl+b _ |xargs xdotool key }
+tmux(){ t tmux; tmctl percent o quotedbl }
+emacs(){ sm Shift+e }
+tile_main_left(){ sm f l f l Return y Return }
+tile_mix(){ sm f l e m y Return }
+tile_space(){ sm f l e s Return y Return }
